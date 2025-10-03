3eb71990207209287bc7968bd0609240581f60e6
shantanuuchak <51011922+shantanuuchak@users.noreply.github.com>
2025-10-03 18:17:01 +0530

    fixed status update bug

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,9 @@ import './style.css'
 
 // DOM Targetting
 const main = document.querySelector('main');
-const title = document.querySelector('title');
 const titleEl = document.querySelector('.title');
 const userBtn = document.querySelector('button');
 
-console.log(main)
 
 // === Game Logic ===
 function startGame() {
@@ -33,20 +31,18 @@ function refreshGame(text = 'retry') {
     userBtn.textContent = text;
     userBtn?.addEventListener('click', () => {
         location.reload()
+        main?.classList.remove('background-win');
+        main?.classList.remove('background-lose');
     })
-    main?.classList.remove('background-win');
-    main?.classList.remove('background-lose');
 }
 
 startGame()
     .then(() => {
-        title.textContent = "you gussed it right!"
-        main.classList.add('background-win')
+        main?.classList.add('background-win')
         titleEl.textContent = "you gussed it right!"
         refreshGame('win again')
     })
     .catch(() => {
-        title.textContent = 'a retry won\'t hurt!'
         main?.classList.add('background-lose')
         titleEl.textContent = 'a retry won\'t hurt!'
         refreshGame('play again')
