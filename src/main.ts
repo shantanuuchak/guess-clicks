856d6fa87563c5253def426ba2f35818ba2f5d63
shantanuuchak <51011922+shantanuuchak@users.noreply.github.com>
2025-10-03 17:28:45 +0530

    logic refactoring in promises

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,48 @@
 import './style.css'
 
 // DOM Targetting
-const main = document.querySelector('main')!;
+const main = document.querySelector('main');
+const title = document.querySelector('title');
+const titleEl = document.querySelector('.title');
 const userBtn = document.querySelector('button');
 
-
-userBtn?.addEventListener('click', () => {
-    userClicks += 1;
-    console.log("Secret value is:", secretValue)
-    console.log("User clicks are:", userClicks)
-})
-
 // === Game Logic ===
-const secretValue = 5;
-let userClicks = 0;
+function startGame() {
+    const secretValue = 5;
+    let userClicks = 0;
 
-setTimeout(() => {
-    if (userClicks === secretValue) {
-        main.style.backgroundColor = 'green';
-    } else {
-        main.style.backgroundColor = 'red';
-    }
+    userBtn?.addEventListener('click', () => {
+        ++userClicks;
+    })
 
-}, 5000)
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (userClicks === secretValue) {
+                resolve();
+            } else {
+                reject();
+            }
+
+        }, 5000)
+    })
+}
+
+function refreshGame(text = 'retry') {
+    userBtn.textContent = text;
+    userBtn?.addEventListener('click', () => {
+        location.reload()
+    })
+}
+
+startGame()
+    .then(() => {
+        title.textContent = "you gussed it right!"
+        titleEl.textContent = "you gussed it right!"
+        refreshGame('win again')
+    })
+    .catch(() => {
+        title.textContent = 'a retry won\'t hurt!'
+        titleEl.textContent = 'a retry won\'t hurt!'
+        refreshGame()
+
+    })
