cbe38b9e8786bfb79df15b2891358ea09c2c9290
shantanuuchak <51011922+shantanuuchak@users.noreply.github.com>
2025-10-03 15:51:34 +0530

    add basic logic done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,25 @@
 import './style.css'
+
+// DOM Targetting
+const main = document.querySelector('main')!;
+const userBtn = document.querySelector('button');
+
+
+userBtn?.addEventListener('click', () => {
+    userClicks += 1;
+    console.log("Secret value is:", secretValue)
+    console.log("User clicks are:", userClicks)
+})
+
+// === Game Logic ===
+const secretValue = 5;
+let userClicks = 0;
+
+setTimeout(() => {
+    if (userClicks === secretValue) {
+        main.style.backgroundColor = 'green';
+    } else {
+        main.style.backgroundColor = 'red';
+    }
+
+}, 5000)
